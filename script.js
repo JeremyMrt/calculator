c078b680d4d2d49d5c1c8de47c0d742a299e389d
Jérémy Martin <114619755+JeremyMrt@users.noreply.github.com>
2022-12-02 09:14:08 +0100

    Creation of files and basic arithmetic

---
+++ script.js
@@ -0,0 +1,17 @@
+// All calculus related functions
+function add(a,b){
+    return a + b
+}
+function substract(a,b) {
+    return a - b
+}
+function multiply(a,b) {
+    return a * b
+}
+function divide(a,b) {
+    return a / b 
+}
+
+function operate(operator, a, b){
+    
+}
